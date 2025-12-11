5258390df08b613e8fb50821b040a44ec3b4e7cc
Charles Hefer <charles.hefer@agresearch.co.nz>
2025-12-11 15:51:23 +1300

    pinned the snakemake and slurm-executor-plugin versions

--- config/setup.sh
+++ config/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-conda create -p ./conda-env -c conda-forge -c bioconda -y snakemake mamba coincbc python apptainer snakemake-executor-plugin-slurm
+conda create -p ./conda-env -c conda-forge -c bioconda -y snakemake=9.13.7 mamba coincbc python apptainer snakemake-executor-plugin-slurm=1.9.2
